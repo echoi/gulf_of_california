04509036ce4da3c6341463019f3afdd8cd076d01
Eunseo Choi <eunseo.choi@gmail.com>
2018-06-05 12:43:13 -0500

    started saving work with ASPECT

---
+++ aspect_prescribed_velocity/CMakeFiles/prescribed_velocity.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/echoi2/projects/prescribed_velocity/prescribed_velocity.cc" "/home/echoi2/projects/prescribed_velocity/CMakeFiles/prescribed_velocity.dir/prescribed_velocity.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASPECT_SOURCE_DIR=\"/opt/aspect/aspect\""
+  "BOOST_NO_AUTO_PTR"
+  "DEBUG"
+  "TBB_DO_ASSERT=1"
+  "TBB_USE_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/aspect/deal.II-v9.0.0/include"
+  "/opt/aspect/deal.II-v9.0.0/include/deal.II/bundled"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/include/openmpi"
+  "/opt/aspect/parmetis-4.0.3/include"
+  "/opt/aspect/trilinos-release-12-10-1/include"
+  "/usr/include/suitesparse"
+  "/opt/aspect/petsc-3.7.6/include"
+  "/opt/aspect/oce-OCE-0.18.2/include/oce"
+  "/opt/aspect/p4est-2.0/FAST/include"
+  "/opt/aspect/slepc-3.7.3/include"
+  "/opt/aspect/aspect/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ aspect_prescribed_velocity/CMakeFiles/prescribed_velocity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prescribed_velocity.dir/prescribed_velocity.cc.o"
+  "libprescribed_velocity.pdb"
+  "libprescribed_velocity.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prescribed_velocity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
