5b5b7cca2be9e4c64aa52f1007497a1e510174b4
Eunseo Choi <eunseo.choi@gmail.com>
2018-06-07 16:22:04 -0500

    initT generation working. The model is not.

--- aspect_prescribed_velocity/generate_initT.cc
+++ aspect_prescribed_velocity/generate_initT.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-
+#include <fstream>
 
 int main()
 {
@@ -16,9 +16,13 @@ int main()
     const double cos_alpha_FAR = cos(obliquity_FAR);
     const double obliquity_PAC = 330.0 * M_PI / 180.0;
     const double cos_alpha_PAC = cos(obliquity_PAC);
+    const double YEAR2SEC = 3.1536e7;
     const double v_half = 0.106 / YEAR2SEC; // m/yr to m/s.
+
     const double xt = 100.0e3;
     const double x0 = 150.0e3;
+    const double surface_temperature = 293.0;
+    const double mantle_temperature = 1593.0;
             
     const int nx = 701;
     const int nz = 351;
@@ -27,7 +31,11 @@ int main()
     const double zmin = 0.0e3;
     const double zmax = 350.0e3;
   
-    for( int j = 0; j < ny; ++j ) {
+    std::ofstream initT_file;
+    initT_file.open("goc_initT.txt");
+    initT_file << "# POINTS: " << nx <<" "<< nz << std::endl;
+    initT_file << "# Columns: x y temperature [K]" << std::endl;
+    for( int j = 0; j < nz; ++j ) {
         for (int i = 0; i < nx; ++i) {
             double xn = i*(xmax-xmin)/(nx-1);
             double zn = j*(zmax-zmin)/(nz-1);
@@ -39,7 +47,7 @@ int main()
                 double xr = (x0 + tan_theta * (tan_theta*xn-yn))/(1.0+tan_theta*tan_theta);
                 double yr = tan_theta * (xr-xn) + yn;
                 double dist = sqrt( (xr-xn)*(xr-xn) + (yr-yn)*(yr-yn) );
-                age = dist / (v_half * cos_alpha);
+                age = dist / (v_half * cos_alpha_FAR);
             }
             // If in the NA plate
             else if( xn > xt && zn > -(xn-xt) * tan_delta ) {
@@ -53,13 +61,18 @@ int main()
                 double xr = (x0 + tan_theta * (tan_theta*xs-ys))/(1.0+tan_theta*tan_theta);
                 double yr = tan_theta * (xr-xs) + ys;
                 double yt = tan_theta * (xt-xs) + ys;
-                d_tr = sqrt( (xr-xt)*(xr-xt) + (yr-yt)*(yr-yt) );
-                d_st = sqrt( (xt-xs)*(xt-xs) + (yt-ys)*(yt-ys) );
-                age = d_tr / (v_half * cos_alpha) + d_st / ( v_half * cos_alpha * cos_del_cos_theta);
+                double d_tr = sqrt( (xr-xt)*(xr-xt) + (yr-yt)*(yr-yt) );
+                double d_st = sqrt( (xt-xs)*(xt-xs) + (yt-ys)*(yt-ys) );
+                age = d_tr / (v_half * cos_alpha_FAR) + d_st / ( v_half * cos_alpha_FAR * cos_del_cos_theta);
                 depth = sqrt( (xs-xn)*(xs-xn) + (zs-zn)*(zs-zn) );
             }
             
-            double w = 0.5 * depth / std::sqrt( diffusivity * age );
-            temperature[i] = param.bc.surface_temperature +
-                    (param.bc.mantle_temperature - param.bc.surface_temperature) * std::erf(w);
-            }
+            double w = 0.5 * depth / sqrt( diffusivity * age );
+            double temperature = surface_temperature + ( mantle_temperature - surface_temperature ) * erf(w);
+            initT_file << xn <<" "<< zn <<" "<< temperature << std::endl;
+        }
+    }
+    initT_file.close();
+
+    return 0;
+}
